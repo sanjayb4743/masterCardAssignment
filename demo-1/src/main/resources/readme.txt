49936bffdb34e9ca718424430579b89c0782b227
sanjayb4743 <sanjayb4743@gmail.com>
2020-07-09 23:32:20 -0600

    Adding the documentation

---
+++ demo-1/src/main/resources/readme.txt
@@ -0,0 +1,8 @@
+The current web service is to validate if the route exists.
+
+Below is the end URI :
+
+/connected?origin=org1&destination=dest1
+
+Input are  passed as request parameters.
+The response will be Yes if the route exists and No if the route does not exists.
--- demo-1/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo-1/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,16 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.example.demo.controller.CitiesConnectedController;
 
